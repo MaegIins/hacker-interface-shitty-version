f209df2fe918d6eab9e46f6cfa2246ce7fbd481a
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-07-07 14:53:59 +0200

    Update text.js

--- js/text.js
+++ js/text.js
@@ -5,7 +5,7 @@ let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let zoneTxt = document.getElementById('txt');
 let txt = '';
 let lastpressed = '';
-zoneTxt.innerHTML = "> "+txt + "|";
+zoneTxt.innerHTML = "> " + txt + "|";
 
 document.onkeypress = (e) => {
     if (e.keyCode === 13) {
@@ -19,43 +19,59 @@ document.onkeypress = (e) => {
         if (lastpressed === 224) {
             addCustomWords();
         }
-    }
-    else if (e.key === 'c') {
+    } else if (e.key === 'c') {
         if (lastpressed === 224) {
             clear();
         }
-    }else {
+    } else {
         for (let i = 0; i < Math.floor(Math.random() * 6) + 2; i++) {
             txt = txt + alphabet[Math.floor(Math.random() * alphabet.length)];
         }
         console.log(txt);
     }
 
-    zoneTxt.innerHTML = "> "+txt + "|";
+    zoneTxt.innerHTML = "> " + txt + "|";
     lastpressed = e.keyCode;
 }
 
 
 function addCustomWords() {
-
+    let tmp = false;
     let clear = confirm('Do you want to empty the wordlist?');
     if (clear) {
-        alphabet = [];
+        tmp = true;
+        //alphabet = [];
     }
     let word = prompt('Your word:');
-    if (word !== null) {
-        alphabet.push(" " + word + " ");
-        alert('Word ' + word + ' added');
-    } else {
-        alert('No word added, you suck.');
+    if (word !== null) {  // if the user didn't cancel
+
+        if (tmp) {  // if the user wanted to empty the wordlist
+            //emptu the wordlist
+            alphabet = [];
+            //alert the user
+            alert('Emptied wordlist and Word ' + word + ' added');
+        } else {// if the user didn't empty the wordlist
+            //don't empty the wordlist and alert the user
+            alert('Word ' + word + ' added');
+        }
+    } else {   // if the user cancelled
+        if (tmp) { // if the user wanted to empty the wordlist
+            //don't empty the wordlist and alert the user
+            alert('No word added, and the wordlist haven\'t been emptied because it must have at least one word');
+        } else { // if the user didn't empty the wordlist
+            //don't empty the wordlist and alert the user
+            alert('No word added, you suck.');
+        }
     }
+    //word added to the alphabet
+    alphabet.push(" " + word + " ");
 }
 
-function clear(){
+function clear() {
     let clear = confirm('Do you want to empty the wordlist?');
     if (clear) {
         txt = '';
-        zoneTxt.innerHTML = "> "+txt + "|";
+        zoneTxt.innerHTML = "> " + txt + "|";
         alert('Cleared');
     } else {
         alert('ok.');
