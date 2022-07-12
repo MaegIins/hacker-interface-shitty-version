ee74c4e75c4c81b2905dc5e563af34c54f7cfbdc
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-07-12 15:24:54 +0200

    commentaire

--- js/curseur.js
+++ js/curseur.js
@@ -1,17 +1,22 @@
 `use strict`;
 
-let orientation = false;
+//get th cursor
 let circle = document.getElementById('circle');
+
+//on the mouse move, the cursor will move
 onMouseMove = (e) => {
+    //get the mouse position and set the cursor position on it
     circle.style.left = e.pageX + 27 + 'px';
     circle.style.top = e.pageY + 27 + 'px';
 }
+//
 document.addEventListener('mousemove', onMouseMove);
 
-
+//on click
 document.onclick = (e) => {
 
     let audio = new Audio('assets/clicsound.mp3');
+    //play the sound
     audio.play();
 
 }
