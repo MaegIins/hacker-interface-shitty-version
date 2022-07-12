6ea9f8999047f015cb658bfe751d45c9e446c4cd
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-07-12 09:21:01 +0200

    cursor calibration

--- js/curseur.js
+++ js/curseur.js
@@ -3,8 +3,8 @@
 
 let circle = document.getElementById('circle');
 onMouseMove = (e) => {
-    circle.style.left = e.pageX + 'px';
-    circle.style.top = e.pageY + 'px';
+    circle.style.left = e.pageX+27 + 'px';
+    circle.style.top = e.pageY+27 + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
 
