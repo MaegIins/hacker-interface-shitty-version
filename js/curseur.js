c3a41c633e4029c5593e6525119b2a5e410c25fc
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-07-13 15:20:46 +0200

    Update curseur.js

--- js/curseur.js
+++ js/curseur.js
@@ -15,12 +15,20 @@ let circle = document.getElementById('circle');
 
 //on the mouse move, the cursor will move
 onMouseMove = (e) => {
+    circle.style.visibility = 'visible';
     //get the mouse position and set the cursor position on it
     circle.style.left = e.pageX + 27 + 'px';
     circle.style.top = e.pageY + 27 + 'px';
 }
-//
+
+
+//if mouse is out of the window, the cursor will be hidden
+onmouseout = () => {
+    circle.style.visibility = 'hidden';
+}
+
 document.addEventListener('mousemove', onMouseMove);
+document.addEventListener('mouseout', onmouseout);
 
 //on click
 document.onclick = (e) => {
